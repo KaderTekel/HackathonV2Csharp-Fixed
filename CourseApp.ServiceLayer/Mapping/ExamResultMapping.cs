900a9d6886fc4d7a0b36f48ce70f676b95064303
KaderTekel <kader.ince74@gmail.com>
2025-11-04 19:54:09 +0100

    ExamResultMapping düzenlendi
    ExamResult entity’si ile ilgili tüm DTO sınıfları arasındaki AutoMapper eşleştirmelerini tanımlar.
    
    Eksik GetById ve Update map’leri eklendi, null referans hatalarını önlemek için güvenli erişim operatörleri kullanıldı.

--- CourseApp.ServiceLayer/Mapping/ExamResultMapping.cs
+++ CourseApp.ServiceLayer/Mapping/ExamResultMapping.cs
@@ -4,23 +4,26 @@ using CourseApp.EntityLayer.Entity;
 
 namespace CourseApp.ServiceLayer.Mapping;
 
-public class ExamResultMapping:Profile
+public class ExamResultMapping : Profile
 {
     public ExamResultMapping()
     {
-        CreateMap<ExamResult,GetAllExamResultDto>().ReverseMap();
-        CreateMap<ExamResult,CreateExamResultDto>().ReverseMap();
+        CreateMap<ExamResult, GetAllExamResultDto>().ReverseMap();
+        CreateMap<ExamResult, GetByIdExamResultDto>().ReverseMap();
+        CreateMap<ExamResult, CreateExamResultDto>().ReverseMap();
+        CreateMap<ExamResult, UpdateExamResultDto>().ReverseMap();
         CreateMap<ExamResult, DeleteExamResultDto>().ReverseMap();
+
         CreateMap<ExamResult, GetAllExamResultDetailDto>()
-            .ForMember(dst => dst.StudentName,opt => opt.MapFrom(src => src.Student!.Name))
-            .ForMember(dst => dst.StudentSurname,opt => opt.MapFrom(src => src.Student!.Surname))
-            .ForMember(dst => dst.ExamName,opt => opt.MapFrom(src => src.Exam!.Name))
+            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(src => src.Student != null ? src.Student.Name : string.Empty))
+            .ForMember(dst => dst.StudentSurname, opt => opt.MapFrom(src => src.Student != null ? src.Student.Surname : string.Empty))
+            .ForMember(dst => dst.ExamName, opt => opt.MapFrom(src => src.Exam != null ? src.Exam.Name : string.Empty))
             .ReverseMap();
-       CreateMap<ExamResult,GetByIdExamResultDetailDto>()
-            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(src => src.Student!.Name))
-            .ForMember(dst => dst.StudentSurname, opt => opt.MapFrom(src => src.Student!.Surname))
-            .ForMember(dst => dst.ExamName, opt => opt.MapFrom(src => src.Exam!.Name))
+
+        CreateMap<ExamResult, GetByIdExamResultDetailDto>()
+            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(src => src.Student != null ? src.Student.Name : string.Empty))
+            .ForMember(dst => dst.StudentSurname, opt => opt.MapFrom(src => src.Student != null ? src.Student.Surname : string.Empty))
+            .ForMember(dst => dst.ExamName, opt => opt.MapFrom(src => src.Exam != null ? src.Exam.Name : string.Empty))
             .ReverseMap();
-        CreateMap<ExamResult, MissingMappingClass>();
     }
 }
