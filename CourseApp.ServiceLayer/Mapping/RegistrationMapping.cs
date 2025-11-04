2da8c34f9b8189811cdda000042c221c597566c9
KaderTekel <kader.ince74@gmail.com>
2025-11-04 20:01:11 +0100

    RegistrationMapping classı düzenlendi
    Registration entity’si ile ilgili tüm DTO eşleştirmeleri düzenlendi.
    Eksik/hatalı MissingRegistrationMappingDto kaldırıldı, SRC harf hatası düzeltildi.
    
    Kod okunabilirliği ve AutoMapper uyumu artırıldı.

--- CourseApp.ServiceLayer/Mapping/RegistrationMapping.cs
+++ CourseApp.ServiceLayer/Mapping/RegistrationMapping.cs
@@ -4,23 +4,24 @@ using CourseApp.EntityLayer.Entity;
 
 namespace CourseApp.ServiceLayer.Mapping;
 
-public class RegistrationMapping:Profile
+public class RegistrationMapping : Profile
 {
     public RegistrationMapping()
     {
-        CreateMap<Registration,GetAllRegistrationDto>().ReverseMap();
-        CreateMap<Registration,GetByIdRegistrationDto>().ReverseMap();
-        CreateMap<Registration,CreateRegistrationDto>().ReverseMap();
-        CreateMap<Registration,UpdatedRegistrationDto>().ReverseMap();
-        CreateMap<Registration,DeleteRegistrationDto>().ReverseMap();
-        CreateMap<Registration,GetAllRegistrationDetailDto>()
-            .ForMember(dst => dst.CourseName,opt => opt.MapFrom(src => src.Course!.CourseName))
-            .ForMember(dst => dst.StudentName,opt => opt.MapFrom(src => src.Student!.Name))
+        CreateMap<Registration, GetAllRegistrationDto>().ReverseMap();
+        CreateMap<Registration, GetByIdRegistrationDto>().ReverseMap();
+        CreateMap<Registration, CreateRegistrationDto>().ReverseMap();
+        CreateMap<Registration, UpdatedRegistrationDto>().ReverseMap();
+        CreateMap<Registration, DeleteRegistrationDto>().ReverseMap();
+
+        CreateMap<Registration, GetAllRegistrationDetailDto>()
+            .ForMember(dst => dst.CourseName, opt => opt.MapFrom(src => src.Course!.CourseName))
+            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(src => src.Student!.Name))
             .ReverseMap();
+
         CreateMap<Registration, GetByIdRegistrationDetailDto>()
             .ForMember(dst => dst.CourseName, opt => opt.MapFrom(src => src.Course!.CourseName))
-            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(SRC => SRC.Student!.Name))
+            .ForMember(dst => dst.StudentName, opt => opt.MapFrom(src => src.Student!.Name))
             .ReverseMap();
-        CreateMap<Registration, MissingRegistrationMappingDto>();
     }
 }
