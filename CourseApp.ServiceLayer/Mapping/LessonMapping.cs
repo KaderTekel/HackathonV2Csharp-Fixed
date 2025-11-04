6a761cc1913ef0f8a02922a7b416704fd3032f46
KaderTekel <kader.ince74@gmail.com>
2025-11-04 19:59:01 +0100

    LessonMapping classı düzenlendi
    Lesson entity’si ile ilgili tüm DTO eşleştirmeleri düzenlendi.
    
    Eksik UpdateLessonDto map eklendi, hatalı NonExistentDtoType kaldırıldı, gereksiz using temizlendi.

--- CourseApp.ServiceLayer/Mapping/LessonMapping.cs
+++ CourseApp.ServiceLayer/Mapping/LessonMapping.cs
@@ -1,23 +1,24 @@
 ﻿using AutoMapper;
-using CourseApp.EntityLayer.Dto.InstructorDto;
 using CourseApp.EntityLayer.Dto.LessonDto;
 using CourseApp.EntityLayer.Entity;
 
 namespace CourseApp.ServiceLayer.Mapping;
 
-public class LessonMapping:Profile
+public class LessonMapping : Profile
 {
     public LessonMapping()
     {
         CreateMap<Lesson, GetAllLessonDto>().ReverseMap();
         CreateMap<Lesson, CreateLessonDto>().ReverseMap();
+        CreateMap<Lesson, UpdateLessonDto>().ReverseMap();
         CreateMap<Lesson, DeleteLessonDto>().ReverseMap();
+
         CreateMap<Lesson, GetAllLessonDetailDto>()
-                .ForMember(dst => dst.CourseName,opt => opt.MapFrom(src => src.Course!.CourseName))
-                .ReverseMap();
-        CreateMap<Lesson,GetByIdLessonDetailDto>()
-                .ForMember(dst => dst.CourseName,opt => opt.MapFrom(src => src.Course!.CourseName))
-                .ReverseMap();
-        CreateMap<Lesson, NonExistentDtoType>();
+            .ForMember(dst => dst.CourseName, opt => opt.MapFrom(src => src.Course!.CourseName))
+            .ReverseMap();
+
+        CreateMap<Lesson, GetByIdLessonDetailDto>()
+            .ForMember(dst => dst.CourseName, opt => opt.MapFrom(src => src.Course!.CourseName))
+            .ReverseMap();
     }
 }
