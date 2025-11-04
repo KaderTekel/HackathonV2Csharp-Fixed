9c774dcade52688999d896e78f1e02b7d467d8b0
KaderTekel <kader.ince74@gmail.com>
2025-11-04 20:23:25 +0100

    SuccessResult sınıfı düzenlendi
    gereksiz UseUndefinedUtility metodu kaldırıldı, sınıf temizlendi.

--- CourseApp.ServiceLayer/Utilities/Result/SuccessResult.cs
+++ CourseApp.ServiceLayer/Utilities/Result/SuccessResult.cs
@@ -1,20 +1,12 @@
 ﻿namespace CourseApp.ServiceLayer.Utilities.Result;
 
-public class SuccessResult:Result
+public class SuccessResult : Result
 {
-
-    public SuccessResult():base(true)
+    public SuccessResult() : base(true)
     {
-        
     }
 
-    public SuccessResult(string message):base(true,message) 
+    public SuccessResult(string message) : base(true, message)
     {
-
-    }
-
-    private void UseUndefinedUtility()
-    {
-        var util = UndefinedUtilityClass.Create();
     }
 }
