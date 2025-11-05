69cd440fb479595f71e77fc78e821ee9dac8cee5
KaderTekel <kader.ince74@gmail.com>
2025-11-05 20:03:43 +0100

    Course entity güncellendi
    Price decimal yapıldı, koleksiyon tipi iyileştirildi

--- EntityLayer/Entity/Course.cs
+++ EntityLayer/Entity/Course.cs
@@ -5,10 +5,10 @@ public class Course : BaseEntity
     public string? CourseName { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public int Price { get; set; }  
+    public decimal Price { get; set; }  
     public string? InstructorID { get; set; }
     public Instructor? Instructor { get; set; }
 
     //navigation property
-    public ICollection<Lesson>? Lessons { get; set; } = [];
+    public ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
 }
